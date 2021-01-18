4ab0347353901f22a82c7aee360de9d09c36806f
jamariod <j25ghs99@aol.com>
2021-01-18 10:19:51 -0500

    Initial Commit

---
+++ LocationsNames/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  LocationsNames
+//
+//  Created by Jamario Davis on 1/18/21.
+//
+import CoreLocation
+import MapKit
+import UIKit
+
+class ViewController: UIViewController {
+    private let map: MKMapView = {
+        let map = MKMapView()
+        return map
+    }()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(map)
+        title = "Home"
+        LocationManager.shared.getUserLocation { [weak self] location in
+            DispatchQueue.main.async {
+                guard let strongSelf = self else {
+                    return
+                }
+                strongSelf.addMapPin(with: location)
+            }
+        }
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        map.frame = view.bounds 
+    }
+    func addMapPin(with location: CLLocation) {
+        let pin = MKPointAnnotation()
+        pin.coordinate = location.coordinate
+        map.setRegion(MKCoordinateRegion(center: location.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.7, longitudeDelta: 0.7)), animated: true)
+        map.addAnnotation(pin)
+        LocationManager.shared.resolveLocationName(with: location) { [weak self] locationName in
+            self?.title = locationName
+        }
+    }
+}
+
